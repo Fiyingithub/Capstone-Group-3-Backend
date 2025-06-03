be7021862eb000c1626b63b43805afa763399851
Fiyin20 <142032833+Fiyingithub@users.noreply.github.com>
2025-06-03 12:47:27 +0100

    Made changes

--- Config/db.config.js
+++ Config/db.config.js
@@ -7,5 +7,5 @@ dotenv.config();
 export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
-  logging: Boolean(process.env.DB_LOGGING),
+  logging: true,
 });
