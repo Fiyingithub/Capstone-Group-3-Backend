d0d0bbb0ed22e7ec3cc60c091726a64a70092b56
Fiyin20 <142032833+Fiyingithub@users.noreply.github.com>
2025-06-04 13:40:09 +0100

    Update db.config.js

--- Config/db.config.js
+++ Config/db.config.js
@@ -8,5 +8,5 @@ export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER,
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
   port: Number(process.env.PORT),
-  logging: true,
+  logging: false,
 });
