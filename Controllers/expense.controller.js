6e4b2cd51e30fe02cf3195b91665662d72fa77f4
Omotolani Adekunle <omotolaniadekunle14@gmail.com>
2025-06-13 17:55:21 +0100

    Update expense.controller.js

--- Controllers/expense.controller.js
+++ Controllers/expense.controller.js
@@ -10,7 +10,6 @@ export const createExpense = async (req, res) => {
       amount,
       description,
       category: categories,
-      paymentMethod,
       date,
     });
     res.status(201).json(expense);
@@ -51,7 +50,7 @@ export const getExpense = async (req, res) => {
 export const updateExpense = async (req, res) => {
   try {
     if (req.body.category) {
-      req.body.category =Array.is Array(req.body.category) ? req.body.category :[req.body.category];
+      req.body.category = Array.isArray(req.body.category) ? req.body.category :[req.body.category];
     }
     const [updatedcount] = await Expense.update(req.body, {
      where: {id: req.params.id,
@@ -60,7 +59,7 @@ export const updateExpense = async (req, res) => {
     });
     
     if (updatedcount ===0) return res.status(404).json({ message: 'Expense not found' });
-  const updatedExpense  = await Expense.fndOne({
+  const updatedExpense  = await Expense.findOne({
     where: {id : req.params.id, userId: req.user.id},
   });
 
